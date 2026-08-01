7f6d7b2a93e0a76d318a1491046e4f7a7d9a4824
Jatin <jatinhappy.singh@gmail.com>
2026-08-02 01:20:06 +0530

    Message section and little slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import NavBar from './Components/NavBar'
-import HeroSection from './Sections/HeroSection'
-import { ScrollTrigger } from 'gsap/all'
-import gsap from 'gsap'
+import NavBar from "./Components/NavBar";
+import HeroSection from "./Sections/HeroSection";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+import MessageSection from "./Sections/MessageSection";
+import FlavorSection from "./Sections/FlavorSection";
 
 gsap.registerPlugin(ScrollTrigger);
 function App() {
   return (
     <main>
-      <NavBar/>
-      <HeroSection/>
-      <div className='h-dvh border border-red-500'/>
+      <NavBar />
+      <HeroSection />
+      <MessageSection />
+      <FlavorSection />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
